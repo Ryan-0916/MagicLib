74652a01e834d93390c5f0e1f2ce56d1b5a7bf40
Ryan0916 <ningrui0916@gmail.com>
2025-04-11 19:47:13 +0800

    2025-04-10
    1. 修改 resetChatDialog 方法的逻辑

--- paper/src/main/java/com/magicrealms/magiclib/paper/controller/TestController.java
+++ paper/src/main/java/com/magicrealms/magiclib/paper/controller/TestController.java
@@ -4,8 +4,13 @@ import com.magicrealms.magiclib.common.command.annotations.CommandListener;
 import com.magicrealms.magiclib.common.command.enums.PermissionType;
 import com.magicrealms.magiclib.paper.MagicLib;
 import com.magicrealms.magiclib.paper.dispatcher.MessageDispatcher;
+import com.magicrealms.magiclib.paper.entity.InputValidatorResult;
+import com.magicrealms.magiclib.paper.menu.InputMenu;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * @author Ryan-0916
@@ -18,46 +23,38 @@ public class TestController {
 
     @Command(text = "^test\\s\\S+$", permissionType = PermissionType.PLAYER)
     public void test2(CommandSender sender, String[] args) {
-        MessageDispatcher.getInstance().sendMessage(MagicLib.getInstance(), ((Player) sender),
+        MessageDispatcher.getInstance().sendMessage(MagicLib.getInstance(), sender,
                 args[1]);
     }
 
     @Command(text = "^test$", permissionType = PermissionType.PLAYER)
     public void test(CommandSender sender, String[] args) {
-        MessageDispatcher.getInstance().sendMessage(MagicLib.getInstance(), ((Player) sender), "<yellow>Woo: <gradient:#5e4fa2:#f79459:red>||||||||||||||||||||||||</gradient>");
-
-//        Player player = ((Player) sender);
-
-
-//        player.getWorld().spawnEntity(player.getLocation(), EntityType.values());
-
-
-//        new InputMenu.Builder().plugin(MagicLib.getInstance())
-//                .player((Player) sender)
-//                .customValidator(v -> {
-//                    InputValidatorResult result = new InputValidatorResult();
-//                    if (StringUtils.isBlank(v)) {
-//                        result.setMessage("您为啥输入空啊");
-//                        return result;
-//                    }
-//                    if (v.equals("炼奶")) {
-//                        result.setMessage("你凭啥输入自己的名字啊？");
-//                        return result;
-//                    }
-//                    if (v.equals("宁睿")) {
-//                        result.setMessage("你凭啥输入我的名字啊？");
-//                        return result;
-//                    }
-//                    result.setMessage("你输入的文字符合");
-//                    result.setValidator(true);
-//                    return result;
-//                })
-//                .resultSlot(1)
-//                .itemStack(new ItemStack(Material.STONE))
-//                .cancelTask(() -> {})
-//                .confirmConsumer(e -> MessageDispatcher.getInstance().sendMessage(MagicLib.getInstance(),
-//                        sender, e))
-//                .open();
+        new InputMenu.Builder().plugin(MagicLib.getInstance())
+                .player((Player) sender)
+                .customValidator(v -> {
+                    InputValidatorResult result = new InputValidatorResult();
+                    if (StringUtils.isBlank(v)) {
+                        result.setMessage("您为啥输入空啊");
+                        return result;
+                    }
+                    if (v.equals("炼奶")) {
+                        result.setMessage("你凭啥输入自己的名字啊？");
+                        return result;
+                    }
+                    if (v.equals("宁睿")) {
+                        result.setMessage("你凭啥输入我的名字啊？");
+                        return result;
+                    }
+                    result.setMessage("你输入的文字符合");
+                    result.setValidator(true);
+                    return result;
+                })
+                .resultSlot(1)
+                .itemStack(new ItemStack(Material.STONE))
+                .cancelTask(() -> {})
+                .confirmConsumer(e -> MessageDispatcher.getInstance().sendMessage(MagicLib.getInstance(),
+                        sender, e))
+                .open();
     }
 
 }
